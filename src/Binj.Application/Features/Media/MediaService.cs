90434d34ed037991f6dc7d1d0b29113e01d8d5e4
dalentri <aydenjmason05@gmail.com>
2026-04-02 19:04:49 -0500

    Added todo for future database implementation in MediaService.cs

--- src/Binj.Application/Features/Media/MediaService.cs
+++ src/Binj.Application/Features/Media/MediaService.cs
@@ -4,5 +4,6 @@ public class MediaService : IMediaService
 {
     public string AddMedia(string title) => $"[Mock] Added {title} to the list.";
 
+    //TODO: Add to database
     public string ListMedia() => "[Mock] List of current media saved:";
 }
