b762bb8dc42f1a0ba9d4908c677bc9a1acd1cb1f
dalentri <aydenjmason05@gmail.com>
2026-04-05 10:22:10 -0500

    Added getters and setters for Book entity

--- src/Binj.Domain/Entities/Book.cs
+++ src/Binj.Domain/Entities/Book.cs
@@ -1,3 +1,10 @@
 namespace Binj.Domain.Entities;
 
+public class Book
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public int Page { get; private set; }
 
+    public Book(string title, string page) { }
+}
