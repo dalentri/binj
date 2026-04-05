23724f142f970c59c12ddbac641ea18eb221b79d
dalentri <aydenjmason05@gmail.com>
2026-04-05 10:29:51 -0500

    Added constructor for book entity

--- src/Binj.Domain/Entities/Book.cs
+++ src/Binj.Domain/Entities/Book.cs
@@ -5,6 +5,19 @@ public class Book
     public Guid Id { get; private set; }
     public string Title { get; private set; }
     public int Page { get; private set; }
+    public string Status { get; private set; }
 
-    public Book(string title, string page) { }
+    public Book(string title, int page, string status)
+    {
+        // Check for invalid titles
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            throw new ArgumentException("Title Cannot be empty");
+        }
+
+        Id = Guid.NewGuid();
+        Title = title;
+        Page = page;
+        Status = status;
+    }
 }
