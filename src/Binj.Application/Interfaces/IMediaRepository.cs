823c6c6b663d861844349273a0bac7c7d24014b4
dalentri <aydenjmason05@gmail.com>
2026-04-11 14:10:49 -0500

    Added entity dependency and interface declaration to IMovieRepository.cs

--- src/Binj.Application/Interfaces/IMediaRepository.cs
+++ src/Binj.Application/Interfaces/IMediaRepository.cs
@@ -5,7 +5,7 @@ using Binj.Domain.Entities;
 public interface IMediaRepository<T>
     where T : Media
 {
-    // Nullable types to allow for null values (if they don't exist)
+    // Nullable types (?) to allow for null values (if they don't exist)
     Task<T?> GetByIdAsync(Guid id);
     Task<T?> GetByTitleAsync(string title);
     Task AddAsync(T entity);
--- src/Binj.Application/Interfaces/IMovieRepository.cs
+++ src/Binj.Application/Interfaces/IMovieRepository.cs
@@ -1,1 +1,6 @@
+using Binj.Domain.Entities;
+
 namespace Binj.Application.Interfaces;
+
+// Inherit from IMediaRepository generic
+public interface IMovieRepository : IMediaRepository<Movie> { }
