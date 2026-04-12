d0fc7b5b42e47fce89c67339c702ea4718ba8e59
dalentri <aydenjmason05@gmail.com>
2026-04-12 18:03:12 -0500

    Added documentation to AddBookCommand.cs and its handler

--- src/Binj.Application/Features/Books/AddBookCommand.cs
+++ src/Binj.Application/Features/Books/AddBookCommand.cs
@@ -1,3 +1,4 @@
 namespace Binj.Application.Features.Books;
 
+// Gets passed to the Handle function in the handler
 public record AddBook(string Title, string Page, string Status);
--- src/Binj.Application/Features/Books/AddBookCommandHandler.cs
+++ src/Binj.Application/Features/Books/AddBookCommandHandler.cs
@@ -16,11 +16,13 @@ public class AddBookCommandHandler
         _repository = repository;
     }
 
-    // asynchronously handles the request by
+    // asynchronously handles the request to write to the database
     public async Task Handle(AddBook request)
     {
+        // Collect the vars from the request into their columns
         var book = new Book(request.Title, int.Parse(request.Page), request.Status);
 
+        // Send the book object to the db
         await _repository.AddAsync(book);
     }
 }
