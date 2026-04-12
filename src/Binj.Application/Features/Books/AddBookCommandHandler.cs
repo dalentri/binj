5c676f3e6c40f16c2855d88e02545ad8d15efd98
dalentri <aydenjmason05@gmail.com>
2026-04-12 17:28:04 -0500

    Populated AddBookCommandHandler.cs with create instructions for Infra layer

--- src/Binj.Application/Features/Books/AddBookCommandHandler.cs
+++ src/Binj.Application/Features/Books/AddBookCommandHandler.cs
@@ -1,1 +1,26 @@
+using Binj.Application.Interfaces;
+using Binj.Domain.Entities;
+
 namespace Binj.Application.Features.Books;
+
+public class AddBookCommandHandler
+{
+    // Dependency Injection
+    // We use readonly to ensure it doesnt change after init
+    // "Privately init an immutable repository var"
+    private readonly IBookRepository _repository;
+
+    // Constructor that takes in the passed in repository and assigns to prev init var
+    public AddBookCommandHandler(IBookRepository repository)
+    {
+        _repository = repository;
+    }
+
+    // asynchronously handles the request by
+    public async Task Handle(AddBook request)
+    {
+        var book = new Book(request.Title, int.Parse(request.Page), request.Status);
+
+        await _repository.AddAsync(book);
+    }
+}
