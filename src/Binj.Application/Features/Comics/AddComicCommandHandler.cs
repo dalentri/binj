f2fa113f5deb66d5806e06f4619ad3b33e7e223f
dalentri <aydenjmason05@gmail.com>
2026-04-12 21:49:43 -0500

    Populated AddComicHandler.cs with constructor and async handler function

---
+++ src/Binj.Application/Features/Comics/AddComicCommandHandler.cs
@@ -0,0 +1,25 @@
+using Binj.Application.Interfaces;
+using Binj.Domain.Entities;
+
+namespace Binj.Application.Features.Comics;
+
+public class AddComicHandler
+{
+    private readonly IComicRepository _repository;
+
+    public AddComicHandler(IComicRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(AddComic request)
+    {
+        var comic = new Comic(
+            request.Title,
+            int.Parse(request.Volume),
+            request.Issue,
+            request.Status,
+            request.Rating
+        );
+    }
+}
