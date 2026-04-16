9c303da1690db990943a5e22dfd5d1da587e54f6
dalentri <aydenjmason05@gmail.com>
2026-04-16 13:30:46 -0500

    Added a help menu for --help flag

--- src/Binj.Cli/Menu.cs
+++ src/Binj.Cli/Menu.cs
@@ -4,7 +4,14 @@ public class Menu
 {
     public static void HelpMenu()
     {
+        Console.WriteLine("Usage:");
+        Console.WriteLine("\t Binj [flags]");
+        Console.WriteLine("\t Binj [command]");
+
         Console.WriteLine("Commands");
-        Console.WriteLine("++++++++++++++++++++++");
+        Console.WriteLine("\tadd - Add media");
+        Console.WriteLine("\tremove - Remove media");
+        Console.WriteLine("\tedit - Edit media");
+        Console.WriteLine("\tview - View all media in the database");
     }
 }
