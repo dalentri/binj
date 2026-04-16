96c9c95ea670d4ed3028558ab291c5295b9f2e44
dalentri <aydenjmason05@gmail.com>
2026-04-16 15:48:12 -0500

    Removed specific entity commands and initialized general commands and settings for code reusability

--- src/Binj.Cli/Program.cs
+++ src/Binj.Cli/Program.cs
@@ -1,14 +1,14 @@
 ﻿using Binj.Cli;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Spectre.Console.Cli;
 
 // Create the builder (Handles args, config, and logging)
 var builder = Host.CreateApplicationBuilder(args);
+var app = new CommandApp();
 
-//FIX:
-builder.Services.AddDbContext<BinjDbContext>(options => );
-
+// Commands
+app.Configure(config => { });
 
 // Build the Host
 using IHost host = builder.Build();
-
