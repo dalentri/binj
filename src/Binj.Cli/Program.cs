b93ae52e94160ecd2e656630bd607b95b10e111d
dalentri <aydenjmason05@gmail.com>
2026-04-16 16:39:54 -0500

    Refactored to reinclude specific commands for maintainability

--- src/Binj.Cli/Program.cs
+++ src/Binj.Cli/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Binj.Cli;
+using Binj.Cli.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Spectre.Console.Cli;
@@ -8,7 +9,10 @@ var builder = Host.CreateApplicationBuilder(args);
 var app = new CommandApp();
 
 // Commands
-app.Configure(config => { });
+app.Configure(config =>
+{
+    config.AddCommand<AddMediaCommand<Book, AddBookSettings>>("add-book");
+});
 
 // Build the Host
 using IHost host = builder.Build();
