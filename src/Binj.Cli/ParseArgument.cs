872975f4a87aec1dc7810232d10070ea218240f7
dalentri <aydenjmason05@gmail.com>
2026-04-16 13:51:12 -0500

    Added Spectre.console package and removed code from Parse method in ParseArgument.cs

--- src/Binj.Cli/ParseArgument.cs
+++ src/Binj.Cli/ParseArgument.cs
@@ -3,30 +3,5 @@ namespace Binj.Cli;
 // Parses user command line arguments
 public class ArgumentParser
 {
-    public static void Parse(string[] args)
-    {
-        // First command
-        switch (args[0])
-        {
-            case "add":
-                Console.WriteLine("Adding media to db");
-                break;
-            case "remove":
-                Console.WriteLine("Removing media from database.");
-                break;
-            case "edit":
-                Console.WriteLine("___ has been edited.");
-                break;
-            case "view":
-                Console.WriteLine("Viewing database.");
-                break;
-            case "--help":
-                Menu.HelpMenu();
-                break;
-            // Default
-            default:
-                Console.WriteLine("Invalid command, please retry. Type 'binj --help' for info.");
-                break;
-        }
-    }
+    public static void Parse(string[] args) { }
 }
