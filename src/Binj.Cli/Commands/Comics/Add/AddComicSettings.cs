0f0075cdf5a15464004268f03ddbc28ee221d861
dalentri <aydenjmason05@gmail.com>
2026-04-17 09:28:56 -0500

    Added commands and options for AddComicSettings.cs

---
+++ src/Binj.Cli/Commands/Comics/Add/AddComicSettings.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using Spectre.Console.Cli;
+
+public class AddComicSettings : CommandSettings
+{
+    [CommandArgument(0, "<title>")]
+    [Description("The name of the media")]
+    public string Title { get; set; } = string.Empty;
+
+    [CommandOption("-i|--issue")]
+    [Description("The issue of the comic")]
+    public int Issue { get; set; } = 1;
+
+    [CommandOption("-v|--volume")]
+    [Description("The volume of the comic")]
+    public int Volume { get; set; } = 1;
+
+    [CommandOption("-s|--status")]
+    [Description("Status of consumption")]
+    [DefaultValue("not started")]
+    public string Status { get; set; } = "not started";
+
+    [CommandOption("-r|--rating")]
+    [Description("Rating of the comic")]
+    public int Rating { get; set; } = 5;
+}
