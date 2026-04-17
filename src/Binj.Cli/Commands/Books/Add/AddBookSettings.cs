8f3b0357a9f4e52b852c6539b7dac0a0246dd84e
dalentri <aydenjmason05@gmail.com>
2026-04-17 09:45:55 -0500

    Refactored init; to set; for mutability

--- src/Binj.Cli/Commands/Books/Add/AddBookSettings.cs
+++ src/Binj.Cli/Commands/Books/Add/AddBookSettings.cs
@@ -5,15 +5,15 @@ public class AddBookSettings : CommandSettings
 {
     [CommandArgument(0, "<title>")]
     [Description("The name of the media")]
-    public string Title { get; init; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
 
     [CommandOption("-s|--status")]
     [Description("Your current status of consumption")]
     [DefaultValue("not_started")]
-    public string? Status { get; init; }
+    public string? Status { get; set; }
 
     [CommandOption("-p|--page")]
     [Description("The page you are currently on")]
     [DefaultValue(0)]
-    public int Page { get; init; } = 0;
+    public int Page { get; set; } = 0;
 }
