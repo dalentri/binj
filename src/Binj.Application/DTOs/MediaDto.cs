d1181c23efe45e0ea5d6f2b94a5dd4f6ab1bc1f0
dalentri <aydenjmason05@gmail.com>
2026-04-18 15:00:52 -0500

    Added mediatr package to application layer and implemented GetAllMediaQuery

--- src/Binj.Application/DTOs/MediaDto.cs
+++ src/Binj.Application/DTOs/MediaDto.cs
@@ -1,1 +1,2 @@
+// The safe list of entity info we want to show the user
 public record MediaDto(Guid Id, string Title, string Author, string MediaType);
--- src/Binj.Application/Queries/GetAllMediaQuery.cs
+++ src/Binj.Application/Queries/GetAllMediaQuery.cs
@@ -1,3 +1,4 @@
 using MediatR;
 
+// "I want a list of media dtos"
 public record GetAllMediaQuery() : IRequest<List<MediaDto>>;
