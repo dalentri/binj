9b00eb57984f983f6c052de6dad23a9ad5cee3f0
dalentri <aydenjmason05@gmail.com>
2026-04-17 20:58:36 -0500

    Populated EditBookSettings.cs

---
+++ src/Binj.Cli/Commands/Books/Edit/EditBookSettings.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel;
+using Spectre.Console.Cli;
+
+public class EditBookSettings
+{
+    [CommandArgument(0, "<Id>")]
+    [Description("Id number of the book")]
+    public int Id { get; set; }
+
+    [CommandOption("-t|--title")]
+    [Description("New title of the book")]
+    public string? Title { get; set; }
+
+    [CommandOption("-p|--page")]
+    [Description("New current page number")]
+    public int Page { get; set; }
+
+    [CommandOption("-s|--status")]
+    [Description("New status of the book")]
+    public string? Status { get; set; }
+}
