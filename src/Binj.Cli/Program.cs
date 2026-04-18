2d248306b88c8ca600e874e6cbac3f948d784ebf
dalentri <aydenjmason05@gmail.com>
2026-04-18 12:53:47 -0500

    Fixed sqlite config syntax

--- src/Binj.Cli/Program.cs
+++ src/Binj.Cli/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Binj.Domain.Entities;
 using Binj.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
